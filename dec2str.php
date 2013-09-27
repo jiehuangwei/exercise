cc91a6e3b8f1f9aec1200b5f2f4f01ddf9712f0a
jiehuangwei <jiehuangwei@ec3s.com>
2013-09-27 18:37:53 +0800

    changed

--- dec2str.php
+++ dec2str.php
@@ -8,30 +8,30 @@ $temp = '\xE7\x82\xB9\xE6\xAD\x8C\xEF\xBC\x8C\xE7\x94\xA8\xE5\xBF\x83\xE8\x89\xA
 // 方法一
 function dec2str_1($str)
 {
-	$str = str_replace('\\x','%',$str);
-	$str = urldecode($str);
-	return $str;
+    $str = str_replace('\\x','%',$str);
+    $str = urldecode($str);
+    return $str;
 }
 
 // 方法二
 function dec2str_2($str)
 {
-	$str = str_replace('\\x','',$str);
-	$str = str_split($str,2);
-	$string = '';
-	for($i=0;$i<=count($str);$i++)
-	{
-		if ( hexdec($str[$i]) > 127 )
-		{
-			$v = $str[$i];
-			$_v = isset($str[++$i]) ? $str[$i]: null;
+    $str = str_replace('\\x','',$str);
+    $str = str_split($str,2);
+    $string = '';
+    for($i=0;$i<=count($str);$i++)
+    {
+        if ( hexdec($str[$i]) > 127 )
+        {
+            $v = $str[$i];
+            $_v = isset($str[++$i]) ? $str[$i]: null;
 
-			$string .= chr(hexdec($v)) . chr(hexdec($_v));
-		} else {
-			$string .= chr($v);
-		}
-	}
-	return $string;
+            $string .= chr(hexdec($v)) . chr(hexdec($_v));
+        } else {
+            $string .= chr($v);
+        }
+    }
+    return $string;
 }
 
 // echo dec2str_1($temp);
